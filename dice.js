90fcabb5bd1c38dcdb6477166856f7f10596ebfa
codebully <bookthesevens@gmail.com>
2014-02-08 21:00:11 -0500

    Dice!

--- dice.js
+++ dice.js
@@ -13,6 +13,10 @@
 //
 //    getName()
 
+        //declare player tokens.
+        var playerOneTokens = 0;
+        var playerTwoTokens = 0;
+
     function rollEm()
     {
         //get player one dice:
@@ -22,12 +26,6 @@
         var p1d3 = Math.floor((Math.random()*6)+1);
         var p1d4 = Math.floor((Math.random()*6)+1);
         var p1d5 = Math.floor((Math.random()*6)+1);
-
-        console.log(p1d1);
-        console.log(p1d2);
-        console.log(p1d3);
-        console.log(p1d4);
-        console.log(p1d5);
 
         document.getElementById("p1d1").innerHTML=p1d1;
         document.getElementById("p1d2").innerHTML=p1d2;
@@ -43,19 +41,13 @@
         var p2d4 = Math.floor((Math.random()*6)+1);
         var p2d5 = Math.floor((Math.random()*6)+1);
 
-        console.log(p2d1);
-        console.log(p2d2);
-        console.log(p2d3);
-        console.log(p2d4);
-        console.log(p2d5);
-
         document.getElementById("p2d1").innerHTML=p2d1;
         document.getElementById("p2d2").innerHTML=p2d2;
         document.getElementById("p2d3").innerHTML=p2d3;
         document.getElementById("p2d4").innerHTML=p2d4;
         document.getElementById("p2d5").innerHTML=p2d5;
 
-        //get total:
+        //get total
 
         var playerOneTotal = p1d1+p1d2+p1d3+p1d4+p1d5;
         var playerTwoTotal = p2d1+p2d2+p2d3+p2d4+p2d5;
@@ -63,11 +55,22 @@
         document.getElementById("playerOneTotal").innerHTML=playerOneTotal;
         document.getElementById("playerTwoTotal").innerHTML=playerTwoTotal;
 
+        //calculate scores and give out tokens:
+
+
         if (playerOneTotal > playerTwoTotal){
+
+            playerOneTokens++;
             document.getElementById("rollResult").innerHTML="Player One Wins!";
+            document.getElementById("playerOneTokens").innerHTML=playerOneTokens;
+
         }
         else if (playerOneTotal < playerTwoTotal){
+
+            playerTwoTokens++;
             document.getElementById("rollResult").innerHTML="Player Two Wins!";
+            document.getElementById("playerTwoTokens").innerHTML=playerTwoTokens;
+
         }
         else {
             document.getElementById("rollResult").innerHTML="It's a DRAW!"
